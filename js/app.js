4bfdde3906d83bbb7eb90179ab1cc0aadfd3c768
fauzi27 <44411728+fauzi27@users.noreply.github.com>
2026-02-17 23:15:43 +0800

    update js/app.js

--- js/app.js
+++ js/app.js
@@ -1555,7 +1555,7 @@ Pertanyaan Bos/Kasir: ${message}`;
 
     // 5. TEMBAK DATA KE GOOGLE GEMINI API (Generasi 2.0)
     try {
-        const response = await fetch('https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=' + GEMINI_API_KEY,{
+        const response = await fetch('https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash-lite-preview-02-05:generateContent?key=' + GEMINI_API_KEY, {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({
