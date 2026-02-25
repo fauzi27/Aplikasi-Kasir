c325eee0759cf43073f1a8464c950cfedf7091d9
fauzi27 <44411728+fauzi27@users.noreply.github.com>
2026-02-25 23:53:14 +0800

    update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'sahabat-usahamu-v18.17'; // Ganti nama biar browser reset
+const CACHE_NAME = 'sahabat-usahamu-v18.18'; // Ganti nama biar browser reset
 const STATIC_ASSETS = [
   './', 
   './index.html',
