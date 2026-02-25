7b4d29a00dfc97cf77ad57b4763e32d44beeadc3
fauzi27 <44411728+fauzi27@users.noreply.github.com>
2026-02-25 22:37:58 +0800

    update js/app.js

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-import { auth, db, secondaryAuth } from './firebase.js?v=18.10';
+import { auth, db, secondaryAuth } from './firebase.js?v=18.11';
 import { generateContext, askGroqAI } from './ai-brain.js';
 import { createUserWithEmailAndPassword, signInWithEmailAndPassword, onAuthStateChanged, signOut } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
 import { collection, addDoc, deleteDoc, doc, query, orderBy, onSnapshot, setDoc, getDoc } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
@@ -1910,7 +1910,7 @@ window.addEventListener('offline', updateConnectionStatus);
 // Cek saat pertama kali load
 document.addEventListener('DOMContentLoaded', () => {
     updateConnectionStatus();
-});
+})1
 
 // ================= FITUR AI CHATBOT (FULL SET: BRAIN + UI) =================
 
